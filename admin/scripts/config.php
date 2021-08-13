c1c2108641efe00ff20c62dfcb046febf00e11e4
Omer Mutlu <61661092+tsubasa52@users.noreply.github.com>
2021-08-13 23:00:41 +0100

    Update config.php

--- admin/scripts/config.php
+++ admin/scripts/config.php
@@ -10,14 +10,26 @@ error_reporting(0);
 
 
 
-define("HOST", "remotemysql.com");
-define("USER", "D6xC6ccP7n");
-define("PASSWORD", "JZfTTNZlyW");
-define("DB", "D6xC6ccP7n");
+// define("HOST", "remotemysql.com");
+// define("USER", "D6xC6ccP7n");
+// define("PASSWORD", "JZfTTNZlyW");
+// define("DB", "D6xC6ccP7n");
 
 
-global $conn;
-$conn = mysqli_connect(HOST, USER, PASSWORD, DB);
+//Get Heroku ClearDB connection information
+$cleardb_url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+$cleardb_server = $cleardb_url["host"];
+$cleardb_username = $cleardb_url["user"];
+$cleardb_password = $cleardb_url["pass"];
+$cleardb_db = substr($cleardb_url["path"],1);
+$active_group = 'default';
+$query_builder = TRUE;
+// Connect to DB
+$conn = mysqli_connect($cleardb_server, $cleardb_username, $cleardb_password, $cleardb_db);
+
+
+// global $conn;
+// $conn = mysqli_connect(HOST, USER, PASSWORD, DB);
 
 function current_page($page)
 {
