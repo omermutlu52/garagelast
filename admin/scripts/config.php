37874a19400904f7f6c82ad5556def0f04f6e18c
Omer Mutlu <61661092+tsubasa52@users.noreply.github.com>
2021-08-13 19:51:33 +0100

    Initial commit

---
+++ admin/scripts/config.php
@@ -0,0 +1,38 @@
+<?php
+ob_start();
+session_start();
+error_reporting(0);
+
+define("HOST", "localhost");
+define("USER", "root");
+define("PASSWORD", "");
+define("DB", "garage");
+
+global $conn;
+$conn = mysqli_connect(HOST, USER, PASSWORD, DB);
+
+function current_page($page)
+{
+    $current_page = basename($_SERVER['PHP_SELF']);
+    if ($current_page == $page) {
+        echo "active";
+    }
+}
+
+function check_status($status)
+{
+    if ($status == 0) {
+        $status = "<span class='badge bg-success'>Active</span>";
+    } else {
+        $status = "<span class='badge bg-danger'>Blocked</span>";
+    }
+    return $status;
+}
+
+function numberOfRecords($conn, $table)
+{
+    $query = "SELECT * FROM " . $table;
+    $result = mysqli_query($conn, $query);
+    $num_rows = mysqli_num_rows($result);
+    echo $num_rows;
+}
