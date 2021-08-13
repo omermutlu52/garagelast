841f687b68c0d6c20b15d54ebdb62e2d5f569a9b
Omer Mutlu <61661092+tsubasa52@users.noreply.github.com>
2021-08-13 23:23:39 +0100

    Update invoice.php

--- pdf/files/invoice.php
+++ pdf/files/invoice.php
@@ -24,8 +24,17 @@
  * @since 2009-04-16
  */
 
+//Get Heroku ClearDB connection information
+$cleardb_url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+$cleardb_server = $cleardb_url["host"];
+$cleardb_username = $cleardb_url["user"];
+$cleardb_password = $cleardb_url["pass"];
+$cleardb_db = substr($cleardb_url["path"],1);
+$active_group = 'default';
+$query_builder = TRUE;
+// Connect to DB
+$conn = mysqli_connect($cleardb_server, $cleardb_username, $cleardb_password, $cleardb_db);
 
-$conn = mysqli_connect("localhost", "root", "", "garage");
 $id = $_GET["id"];
 $bookingSQL = "SELECT * FROM gp_booking WHERE id = '$id'";
 $bookingResult = $conn->query($bookingSQL);
